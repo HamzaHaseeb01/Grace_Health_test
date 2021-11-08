40b5110db770501c416ab412b517dde669b32092
HamzaHaseeb01 <hamza.haseeb001@gmail.com>
2021-11-08 14:41:30 -0800

    changes in docker files

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "mysqldb",
   user: "root",
   password: "",
-  database:'football_results',
+  database: 'football_results',
   dialect: "mysql",
-  operatorsAliases:false
+  operatorsAliases: false
 });
-export const setUpDatabase=()=>{
- con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected with database");
-});
+export const setUpDatabase = () => {
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected with database");
+  });
 }
-export default setUpDatabase;
+export default setUpDatabase
