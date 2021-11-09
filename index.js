390704e7d79ec3eb2766c4ba3defe061799b89ca
HamzaHaseeb01 <hamza.haseeb001@gmail.com>
2021-11-08 17:24:41 -0800

    comment added

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ import router from "./routes/main";
 import seedDB from "./config/seed";
 const app = express();
 
+//connect database
 setupDatabase();
+
+//add seeder
 seedDB();
+
+//routes for api's
 app.use('/v1',router);
+
 app.listen(3000, () => {
     console.log(`server listening at http://localhost:${3000}`)
   })
